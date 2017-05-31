43fcbb5f999bb0ad83c447170f3b81efdaf9d6a2
Esknier <borgjimmy@hotmail.com>
2017-05-31 09:55:31 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // All data is stored in MongoDB (use mongoose)
 // Nothing should break if there is no data in thdatabase
-// You mayy only use the packages discussed in class demos or in this document
+// You may only use the packages discussed in class demos or in this document
 // Users can create an account that stores username, password, user-level (regular user or admin), email, age, and the answers to three multiple choice questions that you come up with.
 // Passwords must be salted and hashed (use bcrypt-nodejs)
 // On the home page, show bar charts for each of the multiple choice questions that shows the percentage of people that selected each answer.
@@ -21,9 +21,7 @@ var express = require('express'),
 var bcrypt = require('bcrypt-nodejs');
 var hash;
 
-bcrypt.hash("bacon", null, null, function(err, hash){
-    console.log(hash);
-});
+
 
 var app = express();
 
@@ -35,8 +33,18 @@ app.use(express.static(path.join(__dirname + '/public')));
 
 var urlencodedParser = bodyParser.urlencoded({
   extended: true
-})
+});
+
+bcrypt.hash("bacon", null, null, function(err, hash){
+    console.log(hash);
+});
 
 app.get('/', route.index);
 
+app.get('/create', route.create);
+
+//app.get('/details/:id', route.details);
+app.post('/create', urlencodedParser, route.createUser);
+//app.post('/edit/:id', urlencodedParser, route.editUser);
+app.get('/delete/:id', route.delete);
 app.listen(3000);
