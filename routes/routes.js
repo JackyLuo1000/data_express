6e2fb08bf5c3383b98a69d4f2bf34144000b17fa
Esknier <borgjimmy@hotmail.com>
2017-06-06 10:28:22 -0600

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -23,53 +23,45 @@ mdb.once('open', function (callback) {
 
 });
 
-var userSchema = mongoose.Schema({
-  username: String,
-  email: String,
-  password: String,
-  age: String,
-  level: String,
-  question1: String,
-  question2: String,
-  question3: String
-});
-
 var User = require('../models/user-model');
-
+var error = '';
 
 exports.index = function (req, res) {
-  var oldDate = new Date();
+//    var datetime = new Date();
+//    var olddate = new Date();
+//    console.log(datetime);
+//    if(req.cookies.lastvisited === datetime) {
+//        res.cookie('lastVisited', datetime)
+//    } else {
+//        olddate = req.cookies.lastvisited
+//        res.cookie('lastVisited', datetime)
+//    }
+    var oldDate = new Date();
     oldDate = req.app.get('oldDate')
     console.log("Routes: " + oldDate)
-  User.find(function (err, user) {
-    if (err) return console.error(err);
-    res.render('index', {
-      title: 'User List',
-      people: user,
-      q1a1: q1a1,
-      q1a2: q1a2,
-      q1a3: q1a3,
-      q1a4: q1a4,
-      q2a1: q2a1,
-      q2a2: q2a2,
-      q2a3: q2a3,
-      q2a4: q2a4,
-      q3a1: q3a1,
-      q3a2: q3a2,
-      q3a3: q3a3,
-      q3a4: q3a4,
-      totalq1: totalq1,
-      totalq2: totalq2,
-      totalq3: totalq3,
-      date: 'Last Visted on ' + req.app.get('oldDate') + '.'
+    User.find(function (err, user) {
+        if (err) return console.error(err);
+        res.render('index', {
+            title: 'User List',
+            people: user,
+            q1a1: q1a1,
+            q1a2: q1a2,
+            q1a3: q1a3,
+            q1a4: q1a4,
+            q2a1: q2a1,
+            q2a2: q2a2,
+            q2a3: q2a3,
+            q2a4: q2a4,
+            q3a1: q3a1,
+            q3a2: q3a2,
+            q3a3: q3a3,
+            q3a4: q3a4,
+            totalq1: totalq1,
+            totalq2: totalq2,
+            totalq3: totalq3,
+            date: 'Last Visted on ' + req.app.get('oldDate') + '.'
+        });
     });
-  });
-};
-
-exports.create = function (req, res) {
-  res.render('create', {
-      title: 'Add User',
-  });
 };
 
 exports.login = function (req, res) {
@@ -90,23 +82,31 @@ exports.login = function (req, res) {
             });
         } else {
             req.session.error = 'Authentication failed, please check your ' + ' username and password.';
-            res.redirect('/');
+            res.render('login', {
+                error: error
+            });
         }
     });
 }
 
+exports.create = function (req, res) {
+    res.render('create', {
+        title: 'Add User'
+    });
+};
+
 exports.createUser = function (req, res) {
-  var user = new User({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-    age: req.body.age,
-    level: req.body.level,
-    question1: req.body.question1,
-    question2: req.body.question2,
-    question3: req.body.question3
-  });
-  if(user.question1 == "Mufasa"){
+    var user = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        age: req.body.age,
+        level: req.body.level,
+        question1: req.body.question1,
+        question2: req.body.question2,
+        question3: req.body.question3
+    });
+    if(user.question1 == "Mufasa"){
     q1a1++;
     totalq1++;
   }else if(user.question1 == "Simba"){
@@ -145,196 +145,194 @@ exports.createUser = function (req, res) {
     q3a4++;
     totalq3++;
   }
-  user.save(function (err, user) {
-    if (err) return console.error(err);
-    console.log(req.body.username + ' added');
-  });
-  res.redirect('/index');
+    user.save(function (err, user) {
+        if (err) return console.error(err);
+        console.log(req.body.username + ' added');
+    });
+    res.redirect('/index');
 };
 
 exports.edit = function (req, res) {
-  User.findById(req.params.id, function (err, user) {
-    if (err) return console.error(err);
-    res.render('edit', {
-      title: 'Edit User',
-      user: user,
+    User.findById(req.params.id, function (err, user) {
+        if (err) return console.error(err);
+        res.render('edit', {
+            title: 'Edit User',
+            user: user
+        });
     });
-  });
 };
 
-
-exports.editPerson = function (req, res) {
-  User.findById(req.params.id, function (err, user) {
-    if (err) return console.error(err);
-    
-    if(user.question1 == "Mufasa"){
-          q1a1--;
-          totalq1--;
-     }else if(user.question1 == "Simba"){
-          q1a2--;
-          totalq1--;
-     }else if(user.question1 == "Scar"){
-         totalq1--;
-         q1a3--;
-     }else if(user.question1 == "Nala"){
-         totalq1--;
-         q1a4--;
-     }
-     if(user.question2 == "Leonardo Da Vinci"){
-         q2a1--;
-         totalq2--;
-     }else if(user.question2 == "Van Gogh"){
-         q2a2--;
-         totalq2--;
-     }else if(user.question2 == "Frida Kahlo"){
-         q2a3--;
-         totalq2--;
-     }else if(user.question2 == "Claude Monet"){
-         q2a4--;
-         totalq2--;
-     }
-     if(user.question3 == "Luigi"){
-         q3a1--;
-         totalq3--;
-     }else if(user.question3 == "Wario"){
-         q3a2--;
-         totalq3--;
-     }else if(user.question3 == "Jumpman"){
-         q3a3--;
-         totalq3--;
-     }else if(user.question3 == "Hiro"){
-         q3a4--;
-         totalq3--;
-     }
-     if(totalq1 < 0 || totalq2 < 0 || totalq3 < 0){
-        totalq1 = 0;
-        totalq2 = 0;
-        totalq3 = 0;
-    }
-    
-    user.username= req.body.username;
-    user.email= req.body.email;
-    user.password= req.body.password;
-    user.age= req.body.age;
-    user.level= req.body.level;
-    user.question1= req.body.question1;
-    user.question2= req.body.question2;
-    user.question3= req.body.question3;
-    
-    if(user.question1 == "Mufasa"){
-        q1a1++;
-        totalq1++;
-    }else if(user.question1 == "Simba"){
-        q1a2++;
-        totalq1++;
-    }else if(user.question1 == "Scar"){
-        totalq1++;
-        q1a3++;
-    }else if(user.question1 == "Nala"){
-        totalq1++;
-        q1a4++;
-    }
-    if(user.question2 == "Leonardo Da Vinci"){
-        q2a1++;
-        totalq2++;
-    }else if(user.question2 == "Van Gogh"){
-        q2a2++;
-        totalq2++;
-    }else if(user.question2 == "Frida Kahlo"){
-        q2a3++;
-        totalq2++;
-    }else if(user.question2 == "Claude Monet"){
-        q2a4++;
-        totalq2++;
-    }
-    if(user.question3 == "Luigi"){
-        q3a1++;
-        totalq3++;
-    }else if(user.question3 == "Wario"){
-        q3a2++;
-        totalq3++;
-    }else if(user.question3 == "Jumpman"){
-        q3a3++;
-        totalq3++;
-    }else if(user.question3 == "Hiro"){
-        q3a4++;
-        totalq3++;
-    }
-    
-    user.save(function (err, person) {
-      if (err) return console.error(err);
-      console.log(req.body.name + ' updated');
+exports.editUser = function (req, res) {
+    User.findById(req.params.id, function (err, user) {
+        if (err) return console.error(err);
+        
+        if(user.question1 == "Mufasa"){
+            q1a1--;
+            totalq1--;
+        }else if(user.question1 == "Simba"){
+            q1a2--;
+            totalq1--;
+        }else if(user.question1 == "Scar"){
+            totalq1--;
+            q1a3--;
+        }else if(user.question1 == "Nala"){
+            totalq1--;
+            q1a4--;
+        }
+        if(user.question2 == "Leonardo Da Vinci"){
+            q2a1--;
+            totalq2--;
+        }else if(user.question2 == "Van Gogh"){
+            q2a2--;
+            totalq2--;
+        }else if(user.question2 == "Frida Kahlo"){
+            q2a3--;
+            totalq2--;
+        }else if(user.question2 == "Claude Monet"){
+            q2a4--;
+            totalq2--;
+        }
+        if(user.question3 == "Luigi"){
+            q3a1--;
+            totalq3--;
+        }else if(user.question3 == "Wario"){
+            q3a2--;
+            totalq3--;
+        }else if(user.question3 == "Jumpman"){
+            q3a3--;
+            totalq3--;
+        }else if(user.question3 == "Hiro"){
+            q3a4--;
+            totalq3--;
+        }
+        if(totalq1 < 0 || totalq2 < 0 || totalq3 < 0){
+            totalq1 = 0;
+            totalq2 = 0;
+            totalq3 = 0;
+        }
+        
+        user.username = req.body.username;
+        user.email = req.body.email;
+        user.password = req.body.password;
+        user.age = req.body.age;
+        user.question1 = req.body.question1;
+        user.question2 = req.body.question2;
+        user.question3 = req.body.question3;
+        
+        if(user.question1 == "Mufasa"){
+            q1a1++;
+            totalq1++;
+        }else if(user.question1 == "Simba"){
+            q1a2++;
+            totalq1++;
+        }else if(user.question1 == "Scar"){
+            totalq1++;
+            q1a3++;
+        }else if(user.question1 == "Nala"){
+            totalq1++;
+            q1a4++;
+        }
+        if(user.question2 == "Leonardo Da Vinci"){
+            q2a1++;
+            totalq2++;
+        }else if(user.question2 == "Van Gogh"){
+            q2a2++;
+            totalq2++;
+        }else if(user.question2 == "Frida Kahlo"){
+            q2a3++;
+            totalq2++;
+        }else if(user.question2 == "Claude Monet"){
+            q2a4++;
+            totalq2++;
+        }
+        if(user.question3 == "Luigi"){
+            q3a1++;
+            totalq3++;
+        }else if(user.question3 == "Wario"){
+            q3a2++;
+            totalq3++;
+        }else if(user.question3 == "Jumpman"){
+            q3a3++;
+            totalq3++;
+        }else if(user.question3 == "Hiro"){
+            q3a4++;
+            totalq3++;
+        }
+        
+        user.save(function (err, person) {
+            if (err) return console.error(err);
+            console.log(req.body.name + ' updated');
+        });
     });
-  });
-  res.redirect('/index');
+    res.redirect('/index');
 };
 
 exports.delete = function (req, res) {
-  User.findByIdAndRemove(req.params.id, function (err, user) {
-    if(user.question1 == "Mufasa"){
-    q1a1--;
-    totalq1--;
-  }else if(user.question1 == "Simba"){
-    q1a2--;
-    totalq1--;
-  }else if(user.question1 == "Scar"){
-    totalq1--;
-    q1a3--;
-  }else if(user.question1 == "Nala"){
-    totalq1--;
-    q1a4--;
-  }
-  if(user.question2 == "Leonardo Da Vinci"){
-    q2a1--;
-    totalq2--;
-  }else if(user.question2 == "Van Gogh"){
-    q2a2--;
-    totalq2--;
-  }else if(user.question2 == "Frida Kahlo"){
-    q2a3--;
-    totalq2--;
-  }else if(user.question2 == "Claude Monet"){
-    q2a4--;
-    totalq2--;
-  }
-  if(user.question3 == "Luigi"){
-    q3a1--;
-    totalq3--;
-  }else if(user.question3 == "Wario"){
-    q3a2--;
-    totalq3--;
-  }else if(user.question3 == "Jumpman"){
-    q3a3--;
-    totalq3--;
-  }else if(user.question3 == "Hiro"){
-    q3a4--;
-    totalq3--;
-  }
-  if(totalq1 < 0 || totalq2 < 0 || totalq3 < 0){
-    totalq1 = 0;
-    totalq2 = 0;
-    totalq3 = 0;
-  }
-    if (err) return console.error(err);
-    res.redirect('/index');
-  });
+    User.findByIdAndRemove(req.params.id, function (err, user) {
+        if(user.question1 == "Mufasa"){
+            q1a1--;
+            totalq1--;
+        }else if(user.question1 == "Simba"){
+            q1a2--;
+            totalq1--;
+        }else if(user.question1 == "Scar"){
+            totalq1--;
+            q1a3--;
+        }else if(user.question1 == "Nala"){
+            totalq1--;
+            q1a4--;
+        }
+        if(user.question2 == "Leonardo Da Vinci"){
+            q2a1--;
+            totalq2--;
+        }else if(user.question2 == "Van Gogh"){
+            q2a2--;
+            totalq2--;
+        }else if(user.question2 == "Frida Kahlo"){
+            q2a3--;
+            totalq2--;
+        }else if(user.question2 == "Claude Monet"){
+            q2a4--;
+            totalq2--;
+        }
+        if(user.question3 == "Luigi"){
+            q3a1--;
+            totalq3--;
+        }else if(user.question3 == "Wario"){
+            q3a2--;
+            totalq3--;
+        }else if(user.question3 == "Jumpman"){
+            q3a3--;
+            totalq3--;
+        }else if(user.question3 == "Hiro"){
+            q3a4--;
+            totalq3--;
+        }
+        if(totalq1 < 0 || totalq2 < 0 || totalq3 < 0){
+            totalq1 = 0;
+            totalq2 = 0;
+            totalq3 = 0;
+        }
+        if (err) return console.error(err);
+        res.redirect('/index');
+    });
 };
 
 exports.details = function (req, res) {
-  User.findById(req.params.id, function (err, user) {
-    if (err) return console.error(err);
-      res.render('details', {
-          title: user.username + "'s Information",
-          user: user
-      });
-      user.comparePassword('luo123', function(err, isMatch) {
-          console.log('luo123:', isMatch); // -&gt; Password123: true
-      });
+    User.findById(req.params.id, function (err, user) {
+        if (err) return console.error(err);
+        res.render('details', {
+            title: user.username + "'s Information",
+            user: user
+        });
+        user.comparePassword('luo123', function(err, isMatch) {
+            console.log('luo123:', isMatch); // -&gt; Password123: true
+        });
         // test a failing password
-      user.comparePassword('123Password', function(err, isMatch) {
-          console.log('123Password:', isMatch); // -&gt; 123Password: false
-      });
-  });
+        user.comparePassword('123Password', function(err, isMatch) {
+            console.log('123Password:', isMatch); // -&gt; 123Password: false
+        });
+    });
 };
 
 exports.logout = function (req, res) {
@@ -360,15 +358,20 @@ function authenticate(name, pass, fn) {
             if (err) return fn(new Error('cannot find user'));
             user.comparePassword(pass, function(err, isMatch){
                 if(isMatch){
+                    error = "";
                     return fn(null, user)
                 }else{
-                    fn(new Error('invalid password'));
+                    error = 'Invalid Password';
+                    return fn(new Error('Invalid Password'));
                 }
             })
         } else {
+            error = 'No username'
             return fn(new Error('cannot find user'));
         }
     });
+}
+
 function dateFormat (date, fstr, utc) {
   utc = utc ? 'getUTC' : 'get';
   return fstr.replace (/%[YmdHMS]/g, function (m) {
@@ -384,5 +387,4 @@ function dateFormat (date, fstr, utc) {
     // add leading zero if required
     return ('0' + m).slice (-2);
   });
-}
 }
